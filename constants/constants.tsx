4d29174ae1b16d8ef586dd8629c33567d3b2b3f8
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-08-03 18:55:57 +0530

    added new images

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -1,19 +1,19 @@
 export const products = [
-  {
-    id: 1,
-    name: "Vonocef 500",
-    image: "@/assets/images/pp/1.png",
-  },
+  // {
+  //   id: 1,
+  //   name: "Vonocef 500",
+  //   image: "@/assets/images/pp/",  image not available
+  // },
   {
     id: 2,
     name: "Tab Azeep",
-    image: "@/assets/images/2.png",
+    image: "@/assets/images/pp/azeep.jpg",
   },
-  {
-    id: 3,
-    name: "Painpure aqua injection",
-    image: "@/assets/images/3.png",
-  },
+  // {
+  //   id: 3,
+  //   name: "Painpure aqua injection",
+  //   image: "@/assets/images/pp/3.jpg",  image not available
+  // },
   {
     id: 4,
     name: "GRED-RAFT Pregnancy",
@@ -22,76 +22,96 @@ export const products = [
   {
     id: 5,
     name: "GRED-RAFT Reflux",
-    image: "@/assets/images/pp/5.png",
+    image: "@/assets/images/pp/5.jpg",
   },
   {
     id: 6,
     name: "Enofer Xt",
-    image: "@/assets/images/pp/6.png",
+    image: "@/assets/images/pp/6.jpg",
   },
   {
     id: 7,
     name: "Arzino",
-    image: "@/assets/images/pp/7.png",
+    image: "@/assets/images/pp/7.jpg",
   },
   {
     id: 8,
     name: "Koral-Fort",
-    image: "@/assets/images/pp/8.png",
+    image: "@/assets/images/pp/koral-fort.jpg",
   },
   {
     id: 9,
     name: "Multipride",
-    image: "@/assets/images/pp/9.png",
+    image: "@/assets/images/pp/9.jpg",
   },
   {
     id: 10,
     name: "Multipride injection",
-    image: "@/assets/images/pp/3.png",
+    image: "@/assets/images/pp/multipride_injection.jpg",
   },
   {
     id: 11,
     name: "ZyDine",
-    image: "@/assets/images/pp/11.png",
+    image: "@/assets/images/pp/11.jpg",
   },
   {
     id: 12,
     name: "L-Bino",
-    image: "@/assets/images/pp/12.png",
+    image: "@/assets/images/pp/12.jpg",
   },
   {
     id: 13,
     name: "cifipure-lb",
-    image: "@/assets/images/14.png",
+    image: "@/assets/images/pp/14.jpg",
   },
   {
     id: 14,
     name: "painpure-sp",
-    image: "@/assets/images/16.png",
+    image: "@/assets/images/pp/16.jpg",
   },
   {
     id: 15,
     name: "Multipride 5G",
-    image: "@/assets/images/17.png",
+    image: "@/assets/images/pp/17.jpg",
   },
   {
     id: 16,
     name: "cvpure 625",
-    image: "@/assets/images/19.png",
+    image: "@/assets/images/pp/19.jpg",
   },
   {
     id: 17,
     name: "purepanta-dsr",
-    image: "@/assets/images/20.png",
+    image: "@/assets/images/pp/20.jpg",
   },
   {
     id: 18,
     name: "montipure-lc",
-    image: "@/assets/images/21.png",
+    image: "@/assets/images/pp/21.jpg",
   },
   {
     id: 19,
     name: "ribosure",
-    image: "@/assets/images/24.png",
+    image: "@/assets/images/pp/24.jpg",
+  },
+  {
+    id: 20,
+    name: "pridomol-500/650mg tablets",
+    image: "@/assets/images/pp/pridomol.jpg",
+  },
+  {
+    id: 21,
+    name: "pridexone-sb injection",
+    image: "@/assets/images/pp/pridexone-sb_injection.jpg",
+  },
+  {
+    id: 22,
+    name: "pridopain-p_tablets",
+    image: "@/assets/images/pp/pridopain-p_tablets.jpg",
+  },
+  {
+    id: 22,
+    name: "pridopain_ace",
+    image: "@/assets/images/pp/pridopain_ace.jpg",
   },
 ];
